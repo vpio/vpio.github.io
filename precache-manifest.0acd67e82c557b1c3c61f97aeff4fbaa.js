f21fb4ab57bcfb2d724cdf8817cf51673cfc3bd4
Pio Molina <vpio@comcast.net>
2019-12-04 22:07:31 -0600

    Updates

---
+++ precache-manifest.0acd67e82c557b1c3c61f97aeff4fbaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac6aafc2e321e9c3e23f8686f7f27c2",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "4831b60040303d85267e",
+    "url": "/myapp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "038f8ef07fbb2a6af33b",
+    "url": "/myapp/static/css/main.73b5db16.chunk.css"
+  },
+  {
+    "revision": "4831b60040303d85267e",
+    "url": "/myapp/static/js/2.fd8c0898.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/myapp/static/js/2.fd8c0898.chunk.js.LICENSE"
+  },
+  {
+    "revision": "038f8ef07fbb2a6af33b",
+    "url": "/myapp/static/js/main.43a1291e.chunk.js"
+  },
+  {
+    "revision": "20423a7ee7c5aa710832",
+    "url": "/myapp/static/js/runtime-main.4259627c.js"
+  }
+]);
