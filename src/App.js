10b644a5fd95631f2dad5de1ea1d68499612b582
Pio Molina <41122916+vpio@users.noreply.github.com>
2021-06-17 13:03:53 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,38 +10,12 @@ function App() {
         <Navbar.Brand>
           Pio Molina
         </Navbar.Brand>
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Music Production | Mixing & Mastering | Violin
-          </Navbar.Text>
-        </Navbar.Collapse>
       </Navbar>
       <div className="main">
         <Container>
-          <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/711448027&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+          <h1>Website Under Construction - Come back Soon!</h1>
         </Container>
-        <Container>
-          <h1>Latest</h1>
-          <p>
-            <Button variant="dark">Full Discography</Button>
-          </p>
-
         </Container>
-        <Nav className="justify-content-center">
-          <Nav.Item>
-            <Nav.Link eventKey="link-3" style={{color: 'grey'}}>
-            <FontAwesomeIcon icon={faSoundcloud} />
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-2" style={{color: 'grey'}}><FontAwesomeIcon icon={faInstagram} /></Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="link-0" style={{color: 'grey'}}><FontAwesomeIcon icon={faFacebook} /></Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-1" style={{color: 'grey'}}><FontAwesomeIcon icon={faTwitter} /></Nav.Link>
-          </Nav.Item>
         </Nav>
       </div>
     </div>
